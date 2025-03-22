b419e57899cbe38c10678d060b4e5f50d8d11d44
JaveedYara72 <javeedymd.iiitg@gmail.com>
2025-03-23 03:41:36 +0530

    added typeorm configs

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,12 +1,21 @@
 // This is our config file, where we get our environment imports from
 import { config } from "dotenv";
+import path from "path";
 
-// import the method
-config();
+// import the method, This will also load the env variables based on the environment
+// Here, based on the command, it will load the env variables from .env.development or .env.production
+config({ path: path.join(__dirname, `../../.env.${process.env.NODE_ENV}`) });
 
-const { PORT, NODE_ENV } = process.env;
+// Here we are getting the environment variables
+const { PORT, NODE_ENV, DB_HOST, DB_PORT, DB_USERNAME, DB_PASSWORD, DB_NAME } =
+    process.env;
 
 export const Config = {
     PORT,
     NODE_ENV,
+    DB_NAME,
+    DB_HOST,
+    DB_USERNAME,
+    DB_PASSWORD,
+    DB_PORT,
 };
