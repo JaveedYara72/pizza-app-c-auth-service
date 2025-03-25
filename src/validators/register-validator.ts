a6da9a84cfa16d3afb188612e5bf2dbe63b98625
JaveedYara72 <javeedymd.iiitg@gmail.com>
2025-03-26 01:59:39 +0530

    added Schema checks and sanitisation

--- src/validators/register-validator.ts
+++ src/validators/register-validator.ts
@@ -1,9 +1,32 @@
-import { body } from "express-validator"; // Import express-validator
+import { checkSchema } from "express-validator";
 
-export default [
-    body("email")
-        .notEmpty()
-        .isEmail()
-        .withMessage("Please Enter a Valid email"),
-    body("firstName").notEmpty().withMessage("Please Enter a First Name"),
-];
+export default checkSchema({
+    email: {
+        trim: true,
+        errorMessage: "Email is required!.",
+        notEmpty: true,
+        isEmail: {
+            errorMessage: "Should be a valid email",
+        },
+    },
+    firstName: {
+        trim: true,
+        errorMessage: "First Name is required",
+        notEmpty: true,
+    },
+    lastName: {
+        trim: true,
+        errorMessage: "Last Name is required",
+        notEmpty: true,
+    },
+    password: {
+        trim: true,
+        errorMessage: "Password is required",
+        isLength: {
+            options: {
+                min: 8,
+            },
+            errorMessage: "Password should atleast be of 8 characters",
+        },
+    },
+});
